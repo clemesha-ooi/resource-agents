6d2a52a1f94fc8b4681d20f05fbe0ac524c69d39
Alex Clemesha <clemesha@ucsd.edu>
2009-04-30 21:16:43 -0700

    Added basic Finite State Machine python model.  The file 'agent_fsm.py' (run by typing ./agent_fsm.py) demostrates how the FSM class might be used to accept message that cause an action to occur, and the state to be changed in the FSM.

---
+++ agent_fsm.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+import time
+import random
+
+from fsm import FSM
+
+
+def do_start(fsm):
+    print "=*=*= do_start =*=*= current_state=%s" % fsm.current_state
+
+def do_reload(fsm):
+    print "==do_reload == current_state=%s" % fsm.current_state
+
+def do_default(fsm):
+    if fsm.input_symbol == "FATAL_BLAH":
+        raise "FATAL_BLAH"
+    print "=do_default= current_state=%s" % fsm.current_state
+    #elif fsm.input_symbol == "RUNNING"
+     #   raise "FATAL_BLAH"
+
+def running_ok(fsm):
+    print "running_ok"
+
+def error(fsm):
+    print "==error== input_symbol=%s" % fsm.input_symbol
+
+
+f = FSM("INIT", [])
+f.set_default_transition(do_default, "INIT")
+#f.add_transition_any("INIT", None, "INIT")
+
+"""
+'START' is the given Action (input_symbol)
+The current_state = 'INIT' of the FSM
+
+(input_symbol, current_state) --> (action, next_state)
+
+"""
+f.add_transition("START", "INIT", do_start, "RUNNING")
+f.add_transition("STOP", "RUNNING", do_start, "INIT")
+
+f.add_transition_list(["START", "INIT", "RELOAD", "RESTART"], "RUNNING", running_ok, "RUNNING")
+
+#print f.process("INIT")
+#print "\n\n\n"
+#print f.process("START")
+
+
+STATES = ["UN_INIT" "RUNNING", "STOPPED" "ERROR"]
+ACTIONS = ["INIT", "START", "STOP", "RELOAD", "RESTART", "RUN_SCRIPT", "BLAH_BLAH", "FATAL_BLAH"]
+while 1:
+    time.sleep(2)
+    action = random.choice(ACTIONS)
+    print "\nACTION=>",action, " - process=>",f.process(action)
