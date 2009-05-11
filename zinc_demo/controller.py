d4a98cb1551451e9d5cf3dcb40c25e5de68774cd
Alex Clemesha <clemesha@ucsd.edu>
2009-05-11 12:56:50 -0700

    added demo roles to controller

--- zinc_demo/controller.py
+++ zinc_demo/controller.py
@@ -27,6 +27,7 @@ import time
 
 from twisted.python import log
 from twisted.internet import reactor
+from twisted.internet.utils import getProcessOutput
 
 log.startLogging(sys.stdout)
 
@@ -39,10 +40,16 @@ spec_path_def = os.path.join(magnet.__path__[0], 'spec', 'amqp0-8.xml')
 EXCHANGE = 'agents'
 RESOURCE = 'Controller'
 
+def local_say(to_say):
+    d = getProcessOutput('/usr/bin/say', ['-v', 'Fred', to_say])
+    return
+
 class ManagedAgent(object):
     name = None
     id = None
     last_heartbeat = None
+    disk_usage = None
+    volume = None
 
     def __init__(self, name, id):
         self.name = name
@@ -72,6 +79,7 @@ class AgentController(pole.Role):
             agent = ManagedAgent(agent_name, agent_id)
         self.registry[agent_id] = agent
         self.agent.known_agents[agent_id] = (self.agent.exchange, agent_id,)
+        local_say('Hello %s' % agent_id)
         self.register_ok(agent_id)
 
     def register_ok(self, agent_id):
@@ -88,6 +96,58 @@ class AgentController(pole.Role):
         agent = self.registry.get(message_object['agent_id'])
         agent.set_diskusage(message_object['payload'])
 
+class DemoRole(pole.Role):
+    name = 'DemoRole'
+
+    def say(self, to_id, to_say):
+        """
+        tell agent to say something
+        """
+        role_message = {'method':'say',
+                        'payload':to_say}
+        self.sendMessage(to_id, role_message) 
+
+    def announce_largest_disk(self):
+        """send message to agent with largest disk
+        """
+        disks = [a.disk_usage for a in self.registry.values()]
+
+        to_say = "I have the largest disk"
+        role_message = {'method':'say',
+                        'payload':to_say}
+        self.sendMessage(to_id, role_message) 
+
+    def set_volume(self, to_id, vol):
+        """
+        set volume of agent 
+        """
+        role_message = {'method':'set_volume',
+                        'payload':str(vol)}
+        self.sendMessage(to_id, role_message)
+
+
+    def get_disk(self, to_id):
+        """
+        get all registered agents disk space
+        """
+        role_message = {'method':'disk',
+                        'payload':''}
+        self.sendMessage(to_id, role_message)
+
+    def action_report_disk(self, message_object):
+        """
+        receive agents disk space
+        """
+        agent_id = message_object['agent_id']
+        agent = self.registry[agent_id]
+        agent.disk_usage = message_object['payload']
+
+    def action_report_volume(self, message_object):
+        """
+        """
+        agent_id = message_object['agent_id']
+        agent = self.registry[agent_id]
+        agent.volume = message_object['payload']
 
 if __name__ == "__main__":
     agent_controller = AgentController()
